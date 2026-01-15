d6145cdb6d1c4a1954ce6f17e9b2fa0994191de8
Shreya Raykar <shreyaraikar2304@gmail.com>
2026-01-15 23:53:31 +0530

    Update test_temp.py

--- test_temp.py
+++ test_temp.py
@@ -1,10 +1,4 @@
 from temp import temperature_convert
 
-def celsius_to_fahrenheit(celsius):
-    return (celsius * 9 / 5) + 32
-
-
-if __name__ == "__main__":
-    temp_c = 25.0   # default Celsius value
-    temp_f = celsius_to_fahrenheit(temp_c)
-    print("Temperature in Fahrenheit:", temp_f)
+def test_temp_1():
+    assert temperature_convert(0) == 32
